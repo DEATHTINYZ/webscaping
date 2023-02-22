20f73c17cc5f84c16bd0ae706cd9f2a0aa725356
DEATHTINYZ <chayutpong.dev@gmail.com>
2023-02-22 16:18:25 +0700

    fix scrape every 15 minutes then store as json and api fetch json

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
+const cron = require('node-cron');
 const express = require('express');
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 const app = express();
 const port = 5000;
 
@@ -10,15 +12,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
-
-app.get('/api/water', async (req, res) => {
+async function scrapeData() {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("https://www.thaiwater.net/water/wl");
+    await page.goto('https://www.thaiwater.net/water/wl');
 
     let data = [];
 
@@ -27,24 +25,26 @@ app.get('/api/water', async (req, res) => {
 
     while (hasNextPage) {
       const pageData = await page.evaluate(() => {
-        const rows = Array.from(document.querySelectorAll("#app table tbody tr"));
+        const rows = Array.from(
+          document.querySelectorAll('#app table tbody tr')
+        );
 
         return rows
           .map((row) => {
-            const th = row.querySelectorAll("th");
-            const td = row.querySelectorAll("td");
-            const svg = row.querySelectorAll("td button span");
+            const th = row.querySelectorAll('th');
+            const td = row.querySelectorAll('td');
+            const svg = row.querySelectorAll('td button span');
             const data = {
-              station: th[0]?.textContent?.trim() || "",
-              location: td[0]?.textContent?.trim().replace(/\s+/g, " ") || "",
-              waterLevel: td[1]?.textContent?.trim() || "",
-              riverBankLevel: td[2]?.textContent?.trim() || "",
-              waterSituation: td[3]?.textContent?.trim() || "",
-              status: td[4]?.textContent?.trim() || "",
-              trend: svg[0]?.innerHTML?.trim() || "",
-              datetime: td[6]?.textContent?.trim() || "",
+              station: th[0]?.textContent?.trim() || '',
+              location: td[0]?.textContent?.trim().replace(/\s+/g, ' ') || '',
+              waterLevel: td[1]?.textContent?.trim() || '',
+              riverBankLevel: td[2]?.textContent?.trim() || '',
+              waterSituation: td[3]?.textContent?.trim() || '',
+              status: td[4]?.textContent?.trim() || '',
+              trend: svg[0]?.innerHTML?.trim() || '',
+              datetime: td[6]?.textContent?.trim() || '',
             };
-            if (Object.values(data).some((value) => value !== "")) {
+            if (Object.values(data).some((value) => value !== '')) {
               return data;
             }
           })
@@ -54,7 +54,7 @@ app.get('/api/water', async (req, res) => {
       data = data.concat(pageData);
 
       const nextButton = await page.$(
-        "#app > main > div > div > div > div:nth-child(4) > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-7.MuiGrid-grid-lg-8 > div:nth-child(3) > div > div.MuiTablePagination-root > div > div > button:nth-child(3)"
+        '#app > main > div > div > div > div:nth-child(4) > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-7.MuiGrid-grid-lg-8 > div:nth-child(3) > div > div.MuiTablePagination-root > div > div > button:nth-child(3)'
       );
       hasNextPage = !(await page.evaluate(
         (nextButton) => nextButton.disabled,
@@ -62,12 +62,38 @@ app.get('/api/water', async (req, res) => {
       ));
 
       await nextButton.click();
-      await page.waitForSelector("#app table tbody tr");
+      await page.waitForSelector('#app table tbody tr');
       pageCounter++;
-      console.log(`Navigated to page ${pageCounter}`);
     }
 
     await browser.close();
+
+    fs.writeFile('data.json', JSON.stringify(data), (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+cron.schedule('*/15 * * * *', () => {
+  scrapeData();
+});
+
+scrapeData();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/api/water', async (req, res) => {
+  try {
+    const rawData = fs.readFileSync('data.json');
+    const data = JSON.parse(rawData);
     res.send(data);
   } catch (error) {
     console.error(error);
