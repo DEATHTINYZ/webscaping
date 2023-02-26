622cc40755f6b166c7c9eb06a7585d92edb6fa07
DEATHTINYZ <78017906+DEATHTINYZ@users.noreply.github.com>
2023-02-26 22:59:42 +0700

    [Backend] change url

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 5000;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://waterdata-table.vercel.app/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
