3998c70d02669ad8038b1afb2e96ce3f49ba2606
DEATHTINYZ <78017906+DEATHTINYZ@users.noreply.github.com>
2023-02-26 23:05:05 +0700

    [Backend] fix cors

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,9 @@ const reservoirData = require('./scraping/Reservoir');
 const app = express();
 const port = 5000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://waterdata-table.vercel.app/');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://waterdata-table.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
